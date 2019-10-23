08d3a1ce44648068bbb3330a9f9fd1f205a42a7c
nbharti08 <nehabharti2012@gmail.com>
2019-10-23 10:21:16 +0530

    Committing all file

---
+++ Integration-Tests/src/test/java/org/sel/com/FirstProject.java
@@ -0,0 +1,38 @@
+package org.sel.com;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstProject {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://www.guru99.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testcaseOne() {
+		WebElement element= driver.findElement(By.xpath("//a[@title=\"Software Testing\"]"));
+		element.click();
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
+
+}
